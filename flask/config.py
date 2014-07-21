c45f083a8dc216fa00a4e307641c895aadeff79e
chudi <elchudi@gmail.com>
2014-07-21 10:49:41 -0300

    Agregado de CORS al logger

--- flask/config.py
+++ flask/config.py
@@ -1,8 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'log.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'log.db')
+SQLALCHEMY_DATABASE_URI = "postgresql://chudi:lalala@localhost/tedxperiments"
+
 SQLALCHEMY_ECHO = False
 SECRET_KEY = '\xfb\x12\xdf\xa1@i\xd6>V\xc0\xbb\x8fp\x16#Z\x0b\x81\xeb\x16'
 DEBUG = True
--- flask/log.py
+++ flask/log.py
@@ -1,15 +1,15 @@
-from datetime import datetime
-from flask import Flask, request, flash, url_for, redirect, \
-     render_template, abort
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-from flask.ext.admin import Admin, BaseView, expose
+from flask.ext.admin import Admin
 from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext.cors import cross_origin
 
 import json
 
 app = Flask(__name__, static_url_path='')
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
 
 class Log(db.Model):
     __tablename__ = 'log'
@@ -21,23 +21,43 @@ class Log(db.Model):
         self.sujeto = sujeto
         self.log = log
 
-
-
 admin = Admin(app)
 admin.add_view(ModelView(Log, db.session))
 
+
 @app.route('/')
-def show_all():
+@cross_origin()
+def default():
     return ""
-    
-@app.route('/createlog', methods = ['POST'])
+
+
+@app.route('/create_log', methods=['POST'])
+@cross_origin()
 def create_log():
-    if not request.json or not 'sujeto' in request.json or not 'log' in request.json:
-        return "request.json"
-    log = Log(request.json['sujeto'], json.dumps(request.json['log']))
+    if not request.json:
+        return "error not json", 400
+    sujeto = request.json['sujeto'] or 'nn'
+    sujeto_log = json.dumps(request.json['log']) or ''
+    log = Log(sujeto, sujeto_log)
     db.session.add(log)
     db.session.commit()
-    return "ok", 201
+    return str(log.id), 201
+
+
+@app.route('/append_log', methods=['POST'])
+@cross_origin()
+def append_log():
+    if not request.json or not 'id' in request.json \
+            or not 'log' in request.json:
+        return "error, missing parameter (id, log) or not json", 400
+    log = Log.query.get(request.json['id'])
+    new_log = json.loads(log.log) or ''
+    new_log.append(json.loads(request.json[log]))
+    log.log = new_log
+    db.session.commit()
+    return "ok", 200
 
 if __name__ == '__main__':
+    app.debug = True
+    app.trap_http_exceptions = True
     app.run()
